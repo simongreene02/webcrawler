ce836bb8a9b1dd5cf91966158016814f8d82ad3a
Simon Greene <simongreene02@gmail.com>
2017-09-09 10:41:22 -0500

    Began adding multithreaded capabilities

--- src/main/java/org/greatworksinc/webcrawler/UrlAccumulator.java
+++ src/main/java/org/greatworksinc/webcrawler/UrlAccumulator.java
@@ -1,27 +1,28 @@
 package org.greatworksinc.webcrawler;
 
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
 
 public class UrlAccumulator {
 	private static final String TERMINAL_TOKEN = "TERMINAL_TOKEN";
 	private static final String USER_AGENT = "";
 	private final HtmlParser htmlParser;
 	private final PageGetter getter;
-	private Queue<String> linkQueue;
+	private final String rootUrl;
 	private final AtomicInteger count = new AtomicInteger();
+	private HashMap<String, Integer> urlDepthMap = Maps.newHashMap();
 	private ExecutorService executorService = Executors.newScheduledThreadPool(3);
 
-	public UrlAccumulator(String rootURL, HtmlParser htmlParser, PageGetter getter) {
+	public UrlAccumulator(String rootUrl, HtmlParser htmlParser, PageGetter getter) {
 		this.htmlParser = htmlParser;
 		this.getter = getter;
-		this.linkQueue = new ConcurrentLinkedQueue<String>();
-		this.linkQueue.offer(rootURL);
+		this.rootUrl = rootUrl;
+		urlDepthMap.put(rootUrl, 0);
 
 	}
 
@@ -30,21 +31,28 @@ public class UrlAccumulator {
 	 * loop through links 5. add links to queue
 	 */
 	public void process() {
-		executorService.execute(new WebsiteChecker());
-		while (count.get() > 0 || !linkQueue.isEmpty()) {
-			if (!linkQueue.isEmpty()) {
-				//Run new thread
-			}
-		}
+		executorService.execute(new WebsiteChecker(rootUrl));
+
 	}
 
 	public class WebsiteChecker implements Runnable {
 
+		private final String url;
+
+		WebsiteChecker(String url) {
+			this.url = url;
+		}
+
 		@Override
 		public void run() {
-			String page = getter.getHTMLFromURL(linkQueue.poll(), USER_AGENT);
-			ImmutableList<String> linkList = htmlParser.getUrls(page);
-			linkQueue.addAll(linkList);
+			int depth = urlDepthMap.get(url);
+			int childDepth = depth + 1;
+			String page = getter.getHTMLFromURL(url, USER_AGENT);
+			ImmutableList<String> urls = htmlParser.getUrls(page);
+			urls.forEach(childUrl -> {
+				urlDepthMap.put(childUrl, depth);
+				executorService.execute(new WebsiteChecker(url));
+			});
 		}
 
 	}
