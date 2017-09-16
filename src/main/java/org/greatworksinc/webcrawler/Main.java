729b90aae100cbed3d79d20f596291397a14c4bb
Simon Greene <simongreene02@gmail.com>
2017-09-16 10:54:26 -0500

    Got UrlAccumulator into a working state.

--- src/main/java/org/greatworksinc/webcrawler/Main.java
+++ src/main/java/org/greatworksinc/webcrawler/Main.java
@@ -3,7 +3,7 @@ package org.greatworksinc.webcrawler;
 public class Main {
 
 	public static void main(String[] args) {
-		new UrlAccumulator().process("https://www.google.com/search?q=car");
+		new UrlAccumulator().process("https://google.github.io/guava/releases/19.0/api/docs");
 
 	}
 
--- src/main/java/org/greatworksinc/webcrawler/UrlAccumulator.java
+++ src/main/java/org/greatworksinc/webcrawler/UrlAccumulator.java
@@ -1,9 +1,10 @@
 package org.greatworksinc.webcrawler;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,20 +13,22 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Maps;
 
 public class UrlAccumulator {
+	private static final Predicate<String> MY_URL_FILTER = url ->  url.endsWith(".html");
 	private static final Logger log = LoggerFactory.getLogger(UrlAccumulator.class);
 	private static final String TERMINAL_TOKEN = "TERMINAL_TOKEN";
 	private static final String USER_AGENT = "";
 	private final HtmlParser htmlParser;
 	private final PageGetter getter;
 	private final AtomicInteger count = new AtomicInteger();
-	private HashMap<String, Integer> urlDepthMap = Maps.newHashMap();
-	private ExecutorService executorService = Executors.newScheduledThreadPool(3);
+	private Map<String, Integer> urlDepthMap = Maps.newConcurrentMap();
+	private ExecutorService executorService = Executors.newFixedThreadPool(2);
 
 	public UrlAccumulator() {
 		this(new HtmlParser(), new PageGetter());
 	}
-	
-	@VisibleForTesting UrlAccumulator( HtmlParser htmlParser, PageGetter getter) {
+
+	@VisibleForTesting
+	UrlAccumulator(HtmlParser htmlParser, PageGetter getter) {
 		this.htmlParser = htmlParser;
 		this.getter = getter;
 	}
@@ -39,21 +42,30 @@ public class UrlAccumulator {
 	public class WebsiteChecker implements Runnable {
 
 		private final String url;
+		private final int depth;
 
 		WebsiteChecker(String url) {
 			this.url = url;
+			this.depth = urlDepthMap.get(url);
 		}
 
 		@Override
 		public void run() {
-			int depth = urlDepthMap.get(url);
-			int childDepth = depth + 1;
 			String page = getter.getHTMLFromURL(url, USER_AGENT);
-			htmlParser.getUrls(page).forEach(childUrl -> {
-				urlDepthMap.put(childUrl, depth);
-				executorService.execute(new WebsiteChecker(childUrl));
-				log.info(childUrl);
-			});
+			htmlParser.getUrls(page).stream().filter(MY_URL_FILTER).forEach(this::addUrlToMap);
+		}
+		
+		private void addUrlToMap(final String childUrl) {
+			String parentUrl = url;
+			if (!urlDepthMap.containsKey(childUrl) && depth < 3) {
+				if (MY_URL_FILTER.test(url)) {
+					parentUrl = parentUrl.substring(0, parentUrl.lastIndexOf('/'));
+				}
+				String newChildUrl = parentUrl+'/'+childUrl;
+				urlDepthMap.put(newChildUrl, depth+1);
+				executorService.execute(new WebsiteChecker(newChildUrl));
+				log.info("depth: {}, url: {}", depth, newChildUrl);
+			}
 		}
 
 	}
